9813fae0814596cfdbdd6756d5c81acab0297717
alanjames00 <alanjames6282@gmail.com>
2024-06-10 00:03:56 +0530

    add: basic routing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+import { Hono } from 'hono';
 
+const app = new Hono();
+
+// route to log the request
+app.get('/log', async (c) => {
+	try {
+		return c.json({
+			message: 'hello',
+		});
+	} catch (e) { }
+});
+
+// route to obtain an apiToken
+app.post('/register', async (c) => {
+	try {
+		return c.json({
+			message: 'hello from register',
+		});
+	} catch (e) {
+		console.log(e);
+		return c.json({ e: e.message }, 500);
+	}
+});
+
+export default app;
